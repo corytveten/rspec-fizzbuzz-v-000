08854eca843dcdd13e9402c893bb612952b8ce53
corytveten <corytveten@gmail.com>
2018-04-09 11:59:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-if int % 3 && int & 5 == 0
-  "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-    elsif int % 5 == 0
-    "Buzz"
+if int % 3 == 0 && int & 5 == 0
+  return "FizzBuzz"
+elsif int % 3 == 0
+  return "Fizz"
+elsif int % 5 == 0
+  return "Buzz"
 
 end
 end
